4e383b218c6a09800031d71d00e95d8975a1b1c3
mash-svg <savithamanigandan10@gmail.com>
2025-06-09 12:42:54 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 
- const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -54,12 +54,12 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 
 
  //Serve static files from React frontend
- app.use(express.static(path.join(__dirname, 'client', 'dist')));
+//  app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-// Catch-all handler: send back React’s index.html file
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
- });
+// // Catch-all handler: send back React’s index.html file
+// app.get('/*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+//  });
 
 
 
