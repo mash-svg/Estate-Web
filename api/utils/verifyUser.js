c6549573e5006ab8af5f1e81d5b37d3570d827a5
mash-svg <savithamanigandan10@gmail.com>
2025-06-15 20:57:27 +0530

    Update verifyUser.js

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,7 +1,7 @@
-const jwt =  require('jsonwebtoken');
-const  errorHandler =  require('./error.js');
+import jwt from 'jsonwebtoken';
+import errorHandler from './error.js';
 
- const verifyToken = (req, res, next) => {
+ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) return next(errorHandler(401, 'Unauthorized'));
@@ -14,4 +14,4 @@ const  errorHandler =  require('./error.js');
   });
 };
 
-module.exports = {verifyToken};
+// module.exports = {verifyToken};
