f4903c49f0373f62f450e63b0be29a010b76a33c
mash-svg <savithamanigandan10@gmail.com>
2025-06-15 20:53:11 +0530

    Update listing.route.js

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const { verifyToken } = require("../utils/verifyUser.js");
+// const express = require('express');
+// const { verifyToken } = require("../utils/verifyUser.js");
 
-const {createListing, deleteListing, updateListing, getListing, getListings} = require('../components/listing.controller')
+// const {createListing, deleteListing, updateListing, getListing, getListings} = require('../components/listing.controller')
+import express from 'express';
 
+import { createListing, deleteListing,getListing,updateListing,getListings} from '../components/listing.controller';
+import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
@@ -10,6 +13,5 @@ router.put('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
 router.get('/get', getListings);
 
+export default router;
 
-
-module.exports = router;
