cbdd3227258633c6f6240b1704a9523df12f17d0
mash-svg <savithamanigandan10@gmail.com>
2025-06-15 20:47:21 +0530

    Update user.controller.js

--- api/components/user.controller.js
+++ api/components/user.controller.js
@@ -1,15 +1,21 @@
-const bcryptjs = require('bcryptjs');
-const User = require('../models/user.model.js');
-const  errorHandler  = require('../utils/error.js');
-const Listing =  require('../models/listing.model.js');
-const test =(req,res)=>{
+// const bcryptjs = require('bcryptjs');
+// const User = require('../models/user.model.js');
+// const  errorHandler  = require('../utils/error.js');
+// const Listing =  require('../models/listing.model.js');
+
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import errorHandler from '../utils/error.js';
+import Listing from '../models/listing.model.js';
+
+export const test =(req,res)=>{
     res.json({
         message:'Hello world!',
     });
 }
 
 
-const updateUser = async (req, res, next) => {
+export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only update your own account!'));
   try {
@@ -42,7 +48,7 @@ console.log(typeof User.findByIdAndUpdate);
 };
 
 
-const deleteUser = async(req,res,next)=>{
+export const deleteUser = async(req,res,next)=>{
   if(req.user.id !== req.params.id){
     return next(errorHandler(401,'You can only delete your own account!'));
 
@@ -59,7 +65,7 @@ const deleteUser = async(req,res,next)=>{
 
 
 
-const getUserListings = async (req, res, next) => {
+export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
       const listings = await Listing.find({ userRef: req.params.id });
@@ -75,7 +81,7 @@ const getUserListings = async (req, res, next) => {
 
 
 
- const getUser = async (req, res, next) =>{
+ export const getUser = async (req, res, next) =>{
   try {
     
     const user = await User.findById(req.params.id);
@@ -93,4 +99,4 @@ const getUserListings = async (req, res, next) => {
 
 
 
-module.exports = {test,updateUser,deleteUser,getUserListings,getUser}
+// module.exports = {test,updateUser,deleteUser,getUserListings,getUser}
