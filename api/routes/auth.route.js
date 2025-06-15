13ff0739a3cf60495dcc299bb298d2495999b0ef
mash-svg <savithamanigandan10@gmail.com>
2025-06-15 20:53:47 +0530

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
 // const express = require('express');
 // const { signup, signin, google, signOut  } = require('../components/auth.controller');
 import express from 'express';
-import { google ,signOut, signin, signup } from '../controllers/auth.controller.js';
+import { google ,signOut, signin, signup } from '../components/auth.controller.js';
 
 const router = express.Router();
 router.post('/signup',signup);
